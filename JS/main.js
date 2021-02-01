a8008671234274d237089c548d87616f12d52531
Elsayedmostafa574 <alsayedb574@gmail.com>
2021-02-01 15:55:19 +0200

    first commit

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        let x = $(window).scrollTop()
+        if(x>500){
+            $('.navbar').css({
+                
+                'background': 'black',
+            
+            })
+          }else{
+           $('.navbar').css({
+             'background': 'transparent',
+             'opacity':'1'
+           })
+          }
+        })
+        var scroll = new SmoothScroll('a[href*="#"]', {
+            speed: 500
+        });
+        jQuery(".fittext").fitText();
+      
+    
+})
