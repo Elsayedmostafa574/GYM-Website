6fdbc05656c75427106ef7c236d4bcc1e90d635e
Elsayedmostafa574 <alsayedb574@gmail.com>
2021-02-01 18:23:38 +0200

    elsayed

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        let x = $(window).scrollTop()
-        if(x>500){
-            $('.navbar').css({
-                
-                'background': 'black',
-            
-            })
-          }else{
-           $('.navbar').css({
-             'background': 'transparent',
-             'opacity':'1'
-           })
-          }
-        })
-        var scroll = new SmoothScroll('a[href*="#"]', {
-            speed: 500
-        });
-        jQuery("#fittext").fitText();
-      
-    
+$(document).ready(function () {
+  $(window).scroll(function () {
+    let x = $(window).scrollTop()
+    if (x > 500) {
+      $('.navbar').css({
+
+        'opacity': '1',
+
+      })
+    } else {
+      $('.navbar').css({
+        'background': 'transparent',
+        'opacity': '0.9'
+      })
+    }
+  })
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500
+  });
+  jQuery("#fittext").fitText();
+
+
 })
